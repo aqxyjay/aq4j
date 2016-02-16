9e7082279fc9b579747486051e29afdef92ba46f
aq <aqxyjay@gmail.com>
2016-02-16 23:09:02 +0800

    add simple socket

---
+++ socket/simple/SimpleClient.java
@@ -0,0 +1,8 @@
+package socket.simple;
+
+/**
+ * Created by aq on 2/16/16.
+ */
+public class SimpleClient {
+
+}
---
+++ socket/simple/SimpleServer.java
@@ -0,0 +1,23 @@
+package socket.simple;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by aq on 2/16/16.
+ * A Simple Socket Server.
+ */
+public class SimpleServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(6000);
+        Socket socket = server.accept();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+
+        System.out.println("Client send: " + reader.readLine());
+        writer.println("Server send back.");
+    }
+
+}
