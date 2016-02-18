795ca38542fe3888a94d90bf937e2a8f40007a2b
aq <aqxyjay@gmail.com>
2016-02-18 23:14:24 +0800

    update server comments -- complete 35%

--- socket/multi/Client.java
+++ socket/multi/Client.java
@@ -43,7 +43,6 @@ public class Client {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
         }
     }
 }
--- socket/multi/Server.java
+++ socket/multi/Server.java
@@ -1,7 +1,19 @@
 package socket.multi;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 /**
  * Created by aq on 2/17/16.
+ * A socket server that support multi clients to connect.
  */
 public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(6000);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
